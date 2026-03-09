f222f432891113648cdac1c5ec15d95dd7a2c4a1
Willian Nogueira <willian.nogueira2016@hotmail.com>
2026-03-09 15:29:51 -0300

    build: prepara settings e dependencias para deploy hibrido

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -149,9 +149,7 @@ DEBUG = int(os.environ.get("DEBUG", default=0))
 
 # 'DJANGO_ALLOWED_HOSTS' should be a single string of hosts with a space between each.
 # For example: 'DJANGO_ALLOWED_HOSTS=localhost 127.0.0.1 [::1]'
-ALLOWED_HOSTS = os.environ.get(
-    "DJANGO_ALLOWED_HOSTS", "localhost 127.0.0.1 [::1]"
-).split(" ")
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'willadmin.pythonanywhere.com']
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
